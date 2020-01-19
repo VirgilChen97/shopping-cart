48a6c529f10fb47ac2f9a17fa5bc26165e709f96
Virgil Chen <virgilchen97@gmail.com>
2020-01-18 18:11:48 -0600

    iventory comes from firebase

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import ElevateAppBar from './component/Appbar';
 import ItemList from './component/ItemList';
 import { Container } from '@material-ui/core';
+import firebase from 'firebase/app'
+import 'firebase/database';
 
-/*const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAaQEsSSJB-hvoosukQW4ILJD25FFFQmBU",
   authDomain: "learn-react-e62f8.firebaseapp.com",
   databaseURL: "https://learn-react-e62f8.firebaseio.com",
@@ -12,24 +14,30 @@ import { Container } from '@material-ui/core';
   messagingSenderId: "523538593380",
   appId: "1:523538593380:web:5831ecab8ba93436449171",
   measurementId: "G-49VNZ2GPXJ"
-};*/
+};
+firebase.initializeApp(firebaseConfig)
 
 const App = () => {
   const [data, setData] = useState({});
   const [cart, setCart] = useState({})
+  const db = firebase.database().ref();
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('/data/products.json');
-      const json = await response.json();
-      setData(json);
+      const productDb = db.child('products');
+      productDb.once('value').then(snapshot => {
+        console.log("The database returns: " + snapshot)
+        if (snapshot.val()) {
+          setData(snapshot.val())
+        }
+      })
     };
     fetchProducts();
   }, []);
 
   return (
     <Container>
-      <ElevateAppBar cart={cart} setCart={setCart} items={data}/>
+      <ElevateAppBar cart={cart} setCart={setCart} items={data} />
       <ItemList cart={cart} setCart={setCart} items={data} />
     </Container>
   );
